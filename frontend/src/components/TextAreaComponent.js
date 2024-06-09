bf3fbeee322ca39c3319f777720c6c08b2414b7e
Anna Lushnikova <anna.loosla@gmail.com>
2024-06-09 14:41:32 -0400

    REmove key, add password input.

--- frontend/src/components/TextAreaComponent.js
+++ frontend/src/components/TextAreaComponent.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 
 const TextAreaComponent = () => {
   const [textAreaValue, setTextAreaValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
 
   useEffect(() => {
     window.api.fetchText()
@@ -9,14 +11,35 @@ const TextAreaComponent = () => {
       .catch(error => console.error('Error fetching data:', error));
   }, []);
 
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
   const handleSave = () => {
     window.api.saveText({ text: textAreaValue })
       .then(() => alert('Text saved successfully'))
       .catch(error => console.error('Error saving data:', error));
   };
 
+  const handleDecrypt = () => {
+    window.api.saveText({ text: "textAreaValue" })
+      .then(() => alert('Text saved successfully'))
+      .catch(error => console.error('Error saving data:', error));
+  };
+
+
   return (
     <div>
+      <label htmlFor="hidden-input">Password: </label>
+      <input
+        id="hidden-input"
+        type="password"
+        value={inputValue}
+        onChange={handleChange}
+      />
+      <br />
+      <button onClick={handleDecrypt}>Decrypt</button>
+      <br />
       <textarea
         value={textAreaValue}
         onChange={(e) => setTextAreaValue(e.target.value)}
