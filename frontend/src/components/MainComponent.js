683a06d5a0383eff05217e82c6f2c0eebab79f4c
Anna Lushnikova <anna.loosla@gmail.com>
2024-07-13 17:59:50 -0400

    Add a button to choose the file.

--- frontend/src/components/MainComponent.js
+++ frontend/src/components/MainComponent.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 const TextAreaComponent = () => {
   const [textAreaValue, setTextAreaValue] = useState('');
   const [password, setPassword] = useState('');
+
+  const fileInputRef = useRef(null);
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      console.log('Selected file:', file);
+    }
+  };
+
+  const openFileDialog = () => {
+    fileInputRef.current.click();
+  };
 
   const handleSave = () => {
     window.api.filesSave({ password: password, content: textAreaValue })
@@ -17,14 +30,25 @@ const TextAreaComponent = () => {
 
   return (
     <div>
-      <label htmlFor="password">Password: </label>
-      <input
-        id="password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Enter password"
-      />
+      <div class="container">
+        <button onClick={openFileDialog}>Choose File</button>
+        <input
+          type="file"
+          ref={fileInputRef}
+          style={{ display: 'none' }}
+          onChange={handleFileUpload}
+        />
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter password"
+          />
+        </div>
+      </div>
       <br />
       <button onClick={handleFetchContent}>Decrypt</button>
       <br />
